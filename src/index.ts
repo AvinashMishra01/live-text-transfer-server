80dc00efeaa69ec07cc5bf1e868b91ce7b7b0895
Vinay Pratap Singh <technicalharvi@gmail.com>
2024-01-11 23:29:45 +0530

    feat: setup of socket server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Socket } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+const isDev = app.settings.env === "development";
+const URL = isDev ? "http://localhost:3000" : process.env.FRONTEND_URL;
+app.use(
+  cors({
+    origin: URL,
+  })
+);
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: URL,
+  },
+});
+
+io.on("connection", (socket: Socket) => {});
+
+httpServer.listen(process.env.PORT || 5000);
